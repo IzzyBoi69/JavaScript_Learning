6dba5e8ed3207df30c73554aad95e7bd043bc192
Soumya <soumyabanerjee2002@gmail.com>
2026-01-25 19:29:20 +0530

    day5 learning done

---
+++ 05/script.js
@@ -0,0 +1,13 @@
+// Logical Operators
+// AND --> All conditions must be true.
+// OR --> either one or more condition should be true
+// NOT --> converts true to false and false to true
+
+const age = 20;
+const gender = 'male';
+
+if (age >= 18 && gender == "male") {
+    console.log(true)
+} else {
+    console.log(false)
+}
